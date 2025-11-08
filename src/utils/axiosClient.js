fcadd8475837ce7cb399603d8f746a079ee82fb0
vipul kumar <bca23161@glbim.ac.in>
2025-11-08 16:04:26 +0530

    remove some REDIS logic

--- src/utils/axiosClient.js
+++ src/utils/axiosClient.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const axiosClient = axios.create({
   baseURL: "https://leetcode-2-ukra.onrender.com",
+  // baseURL: "http://localhost:3000",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
