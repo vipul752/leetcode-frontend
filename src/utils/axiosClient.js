ff7f745a51ee79761becac2c85af22a76401b27d
vipul kumar <bca23161@glbim.ac.in>
2025-11-19 21:28:19 +0530

    add backend of RESUME ANALYSIS(ATS)

--- src/utils/axiosClient.js
+++ src/utils/axiosClient.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  // baseURL: "https://api.codearena.digital",
-  baseURL: "http://localhost:3000",
+  baseURL: "https://api.codearena.digital",
+  // baseURL: "http://localhost:3000",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
